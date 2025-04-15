2424cb23c07e82ac75d836aae2ca5f0362bd9c36
Andrei Ardelean <AndreiArdelean1@users.noreply.github.com>
2025-04-15 13:40:59 +0300

    Reset encryption keys when loading a new login token (#65)
    * Reset encryption keys when loading a login token
    * Move AES_KEY_DEFAULT, AES_IV_DEFAULT to constants

--- custom_components/neakasa/api_encryption.py
+++ custom_components/neakasa/api_encryption.py
@@ -2,9 +2,15 @@ from Cryptodome.Cipher import AES
 from base64 import b64encode, b64decode
 import time
 
+from .const import AES_KEY_DEFAULT, AES_IV_DEFAULT
+
 class APIEncryption:
-    AES_KEY = b"3J74PRUE5TKPJP32"
-    AES_IV = b"QB8GC2X6WK39FF93"
+    def __init__(self):
+        self.resetEncryption()
+
+    def resetEncryption(self):
+        self.aes_key = AES_KEY_DEFAULT
+        self.aes_iv = AES_IV_DEFAULT
 
     async def _pad(self, data):
         """NoPadding-compatible: manually fill up to block size 16 with zero bytes"""
@@ -17,13 +23,13 @@ class APIEncryption:
         return data.rstrip(b'\x00')
 
     async def encrypt(self, plain_text):
-        cipher = AES.new(self.AES_KEY, AES.MODE_CBC, self.AES_IV)
+        cipher = AES.new(self.aes_key, AES.MODE_CBC, self.aes_iv)
         padded = await self._pad(plain_text.encode('utf-8'))
         encrypted = cipher.encrypt(padded)
         return b64encode(encrypted).decode('utf-8')
 
     async def decrypt(self, encrypted_text):
-        cipher = AES.new(self.AES_KEY, AES.MODE_CBC, self.AES_IV)
+        cipher = AES.new(self.aes_key, AES.MODE_CBC, self.aes_iv)
         raw = b64decode(encrypted_text.replace(" ", "+"))
         decrypted = cipher.decrypt(raw)
         return (await self._unpad(decrypted)).decode('utf-8')
@@ -35,6 +41,8 @@ class APIEncryption:
         return await self.encrypt(self._token + '@' + await self._get_timestamp())
 
     async def decodeLoginToken(self, login_token):
+        self.resetEncryption()
+
         decrypted = await self.decrypt(login_token)
         parts = decrypted.split('@')
 
@@ -44,6 +52,6 @@ class APIEncryption:
             self.userid = parts[1]
             self.uid = await self.encrypt(parts[1])
         if len(parts) >= 3:
-            self.AES_KEY = parts[2].encode()
+            self.aes_key = parts[2].encode()
         if len(parts) >= 4:
-            self.AES_IV = parts[3].encode()
+            self.aes_iv = parts[3].encode()
